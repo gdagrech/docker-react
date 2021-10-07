565f76a661ebb742d20fad70aaf7ba0bdb24b446
gdagrech <gdagrech@gmail.com>
2021-10-07 16:30:25 -0300

    updates test 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch
+          Hi Learn React changed
         </a>
       </header>
     </div>
